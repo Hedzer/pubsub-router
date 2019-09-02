5024809e5d10e794ccf53797a3190560b473ad67
Hedzer <hedzer87@gmail.com>
2019-09-02 13:19:57 -0400

    baseline commit before starting over

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = __importDefault(require("./lib/router"));
+exports.default = router_1.default;
---
+++ index.ts
@@ -0,0 +1,4 @@
+
+import Router from './lib/Router';
+
+export default Router;
